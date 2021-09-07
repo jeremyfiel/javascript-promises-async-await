23b3f823ab0df0b955ad214b22edbac56dfdb8b9
Jeremy Fiel <jeremyrfiel@gmail.com>
2021-09-07 12:53:16 -0400

    module 4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import { fetchWithTimeout } from "./services";
-const movies = require("./data/movies.json");
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
+const movies = require('./data/movies.json');
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
-}
+// function fetchMovies() {
+//   const resolveFunction = () => movies;
+//   return fetchWithTimeout(1000).then(resolveFunction);
+// }
 
-const moviePromise = fetchMovies();
-moviePromise.then((results) => {
-  console.log(results);
+// const moviePromise = fetchMovies();
+// moviePromise.then((results) => {
+//   console.log(results);
+// });
+
+const getBooksAndMovies = () => {
+  return Promise.all([fetchBooks, fetchMovies])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch(error => console.log('Error fetching books and movies', error));
+};
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+getBooksAndMoviesPromise.then(results => {
+  console.log('getBooksAndMoviesPromise', results);
+});
+
+const getBooksOrMovies = () => {
+  return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => console.log(results))
+    .catch(error => {
+      console.log('Error waiting for the promise race', error);
+    });
+};
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => {
+  console.log('getBooksOrMoviesPromise', results);
 });
--- src/services.js
+++ src/services.js
@@ -1,3 +1,17 @@
-export default function fetchWithTimeout(delay) {
+export function fetchWithTimeout(delay) {
   return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+export function fetchMovies() {
+  return fetch("./data/movies.json")
+    .then((response) => response.json())
+    .then((movies) => movies)
+    .catch((error) => console.error(error));
+}
+
+export function fetchBooks() {
+  return fetch("./data/books.json")
+    .then((response) => response.json())
+    .then((books) => books)
+    .catch((error) => console.error(error));
 }
