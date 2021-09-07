a6aad6db728b4398287fd23adec67d453d383979
Jeremy Fiel <jeremyrfiel@gmail.com>
2021-09-07 10:32:35 -0400

    module 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export function fetchMovies() {}
+const movies = require("./movies.json");
+const { fetchWithTimeout } = require("./services");
+
+export function fetchMovies() {
+  const resolveFunction = () => movies;
+
+  return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then((results) => console.log(results));
